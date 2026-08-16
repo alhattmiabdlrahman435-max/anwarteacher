15d3931fe7deb2586b69f63e56642708f62babee
f2363692-commits (f2363692-commits) <f2363692@gmail.com>
2026-08-16 04:07:50 +0300

    fix: set ndkVersion to stable installed version 27.0.12077973

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "com.alhattami.anwarteacher"
     compileSdk = 36
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
